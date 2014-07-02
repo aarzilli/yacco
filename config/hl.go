0acb8d61347c266ee923a6ae9746244a2e61a890
ddzuk <ddzuk@litp.localdomain>
2014-07-02 13:57:45 +0200

    added javascript highlighting

--- config/hl.go
+++ config/hl.go
@@ -10,25 +10,25 @@ var RegionMatches = []util.RegionMatch{
 		NameRe: nil,
 	},
 
-	// C / C++ / Java / Go
+	// C / C++ / Java / Go / js
 	util.RegionMatch{
 		NameRe:     regexp.MustCompile(`\.go$`),
 		StartDelim: []rune{'`'}, EndDelim: []rune{'`'}, Escape: 0, Type: util.RMT_STRING,
 	},
 	util.RegionMatch{
-		NameRe:     regexp.MustCompile(`\.(?:c|java|cpp|h|go)$`),
+		NameRe:     regexp.MustCompile(`\.(?:c|java|cpp|h|go|js)$`),
 		StartDelim: []rune{'"'}, EndDelim: []rune{'"'}, Escape: '\\', Type: util.RMT_STRING,
 	},
 	util.RegionMatch{
-		NameRe:     regexp.MustCompile(`\.(?:c|java|cpp|h|go)$`),
+		NameRe:     regexp.MustCompile(`\.(?:c|java|cpp|h|go|js)$`),
 		StartDelim: []rune{'\''}, EndDelim: []rune{'\''}, Escape: '\\', Type: util.RMT_STRING,
 	},
 	util.RegionMatch{
-		NameRe:     regexp.MustCompile(`\.(?:c|java|cpp|h|go)$`),
+		NameRe:     regexp.MustCompile(`\.(?:c|java|cpp|h|go|js)$`),
 		StartDelim: []rune("/*"), EndDelim: []rune("*/"), Escape: rune(0), Type: util.RMT_COMMENT,
 	},
 	util.RegionMatch{
-		NameRe:     regexp.MustCompile(`\.(?:c|java|cpp|h|go)$`),
+		NameRe:     regexp.MustCompile(`\.(?:c|java|cpp|h|go|js)$`),
 		StartDelim: []rune("//"), EndDelim: []rune{'\n'}, Escape: rune(0), Type: util.RMT_COMMENT,
 	},
 
