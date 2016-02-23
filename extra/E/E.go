c9bbc9e15f42272b32090ab70e4217f57aa18541
a <a@kra>
2016-02-23 12:36:31 +0100

    Make E not open a new instance when the file is already open

--- extra/E/E.go
+++ extra/E/E.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/lionkov/go9p/p"
+	"github.com/lionkov/go9p/p/clnt"
 	"io"
 	"os"
 	"strings"
 	"time"
 	"yacco/util"
+	"strconv"
 )
 
 var debug = false
@@ -42,19 +44,38 @@ func main() {
 		path = os.Args[2]
 	}
 	abspath := util.ResolvePath(wd, path)
+	
+	outbufid := ""
+	
+	indexEntries, err := util.ReadIndex(p9clnt)
+	for i := range indexEntries {
+		if indexEntries[i].Path == abspath {
+			outbufid = strconv.Itoa(indexEntries[i].Idx)
+			break
+		}
+	}
+	
+	var ctlfd *clnt.File
+	
+	if outbufid == "" {
+		ctlfd, err = p9clnt.FOpen("/new/ctl", p.ORDWR)
+		util.Allergic(debug, err)
+		ctlln := read(ctlfd)
+		outbufid = strings.TrimSpace(ctlln[:11])
 
-	ctlfd, err := p9clnt.FOpen("/new/ctl", p.ORDWR)
-	util.Allergic(debug, err)
-	ctlln := read(ctlfd)
-	outbufid := strings.TrimSpace(ctlln[:11])
-
-	_, err = fmt.Fprintf(ctlfd, "name %s", abspath)
-	util.Allergic(debug, err)
-	_, err = fmt.Fprintf(ctlfd, "get")
-	util.Allergic(debug, err)
+		_, err = fmt.Fprintf(ctlfd, "name %s", abspath)
+		util.Allergic(debug, err)
+		_, err = fmt.Fprintf(ctlfd, "get")
+		util.Allergic(debug, err)
+	} else {
+		ctlfd, err = p9clnt.FOpen("/"+outbufid+"/ctl", p.ORDWR)
+		util.Allergic(debug, err)
+	}
 
 	if toline != "" {
 		addrfd, err := p9clnt.FOpen("/"+outbufid+"/addr", p.OWRITE)
+		util.Allergic(debug, err)
+		_, err = io.WriteString(addrfd, "0")
 		util.Allergic(debug, err)
 		_, err = io.WriteString(addrfd, toline)
 		util.Allergic(debug, err)
