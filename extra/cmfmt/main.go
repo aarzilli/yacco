a7fd08f15d5d62ac9b54942841007fb8c5a9fa34
a <a@kra>
2023-10-29 10:39:49 +0100

    cmfmt: add more features and test

--- extra/cmfmt/main.go
+++ extra/cmfmt/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
-func getprefix(text string) (prefix, rest string) {
+func getcmtprefix(text string) (prefix, rest string) {
 	i := 0
 	for i < len(text) {
 		if text[i] != ' ' && text[i] != '\t' {
@@ -43,10 +46,53 @@ func getprefix(text string) (prefix, rest string) {
 	return text[:i], text[i:]
 }
 
-func splittext(text string, prefixlen, n int) []string {
+func getitemprefix(text string) (prefix, rest string) {
+	i := 0
+	for i < len(text) {
+		if text[i] != ' ' && text[i] != '\t' {
+			break
+		}
+		i++
+	}
+
+	if i >= len(text) {
+		return "", text
+	}
+
+	if text[i] != '*' && text[i] != '-' {
+		return "", text
+	}
+
+	i++
+
+	if i >= len(text) {
+		return "", text
+	}
+
+	if text[i] != ' ' && text[i] != '\t' {
+		return "", text
+	}
+
+	return text[:i+1], text[i+1:]
+}
+
+func getprefix(text string) (prefix0, prefix1, rest string) {
+	cmtprefix, rest := getcmtprefix(text)
+	itemprefix, rest := getitemprefix(rest)
+	itemprefix1 := []byte(itemprefix)
+	for i := range itemprefix1 {
+		if itemprefix1[i] != ' ' && itemprefix1[i] != '\t' {
+			itemprefix1[i] = ' '
+		}
+	}
+	return cmtprefix + itemprefix, cmtprefix + string(itemprefix1), rest
+}
+
+func splittext(text string, prefixlen0, prefixlen1, n int) []string {
 	b := []byte(text)
 	lastSpace := -1
 	lineStart := 0
+	prefixlen := prefixlen0
 	for i := 0; i < len(b); i++ {
 		if b[i] == ' ' || b[i] == '\t' {
 			lastSpace = i
@@ -55,9 +101,64 @@ func splittext(text string, prefixlen, n int) []string {
 			b[lastSpace] = '\n'
 			lineStart = lastSpace + 1
 			lastSpace = -1
+			prefixlen = prefixlen1
 		}
 	}
 	return strings.Split(string(b), "\n")
+}
+
+func flush(w io.Writer, in string, sz int) {
+	prefix0, prefix1, t := getprefix(in)
+	v := splittext(t, len(prefix0), len(prefix1), sz)
+	prefix := prefix0
+	for _, line := range v {
+		fmt.Fprintf(w, "%s%s\n", prefix, line)
+		prefix = prefix1
+	}
+}
+
+func reflow(in string, sz int) string {
+	lines := strings.Split(in, "\n")
+	outlines := []string{}
+	line := []string{}
+	for i := 0; i < len(lines); i++ {
+		line = append(line, strings.TrimRight(lines[i], " "))
+		if len(line) > 1 {
+			cmt, _ := getcmtprefix(line[0])
+			if cmt != "" && strings.HasPrefix(line[len(line)-1], cmt) {
+				line[len(line)-1] = line[len(line)-1][len(cmt):]
+			}
+		}
+		cur := line[len(line)-1]
+		_, cur = getcmtprefix(cur)
+		if cur == "" || cur[len(cur)-1] == '.' || cur[len(cur)-1] == ',' || cur[len(cur)-1] == ':' || cur[len(cur)-1] == ';' {
+			outlines = append(outlines, strings.Join(line, " "))
+			line = line[:0]
+			continue
+		}
+		if i+1 < len(lines) && len(lines[i+1]) > 0 && len(line) > 0 {
+			cmt, _ := getcmtprefix(line[0])
+			next := lines[i+1]
+			if cmt != "" && strings.HasPrefix(next, cmt) {
+				next = next[len(cmt):]
+			}
+			ch, _ := utf8.DecodeRuneInString(next)
+			if !unicode.IsLetter(ch) && !unicode.IsNumber(ch) {
+				outlines = append(outlines, strings.Join(line, " "))
+				line = line[:0]
+				continue
+			}
+		}
+	}
+	if len(line) > 0 {
+		outlines = append(outlines, strings.Join(line, " "))
+	}
+
+	out := new(bytes.Buffer)
+	for _, line := range outlines {
+		flush(out, line, sz)
+	}
+	return out.String()
 }
 
 func main() {
@@ -66,15 +167,9 @@ func main() {
 		n, _ := strconv.Atoi(os.Args[1])
 		sz = int(n)
 	}
-	s := bufio.NewScanner(os.Stdin)
-	for s.Scan() {
-		prefix, t := getprefix(s.Text())
-		v := splittext(t, len(prefix), sz)
-		for _, line := range v {
-			fmt.Printf("%s%s\n", prefix, line)
-		}
-	}
-	if err := s.Err(); err != nil {
+	buf, err := io.ReadAll(os.Stdin)
+	if err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
+	os.Stdout.Write([]byte(reflow(string(buf), sz)))
 }
---
+++ extra/cmfmt/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestReflow(t *testing.T) {
+	const testdir = "testdata"
+	fis, err := os.ReadDir(testdir)
+	must(err)
+	for _, fi := range fis {
+		buf, _ := os.ReadFile(filepath.Join(testdir, fi.Name()))
+		v := strings.Split(string(buf), "\n====\n")
+		in := v[0]
+		tgt := v[1]
+		t.Run(fi.Name(), func(t *testing.T) { testHelper(t, fi.Name(), in, tgt) })
+	}
+}
+
+func testHelper(t *testing.T, name, in, tgt string) {
+	out := reflow(in, 75)
+	if out != tgt {
+		t.Errorf("wrong output for %s expected\n[%s]\ngot:\n[%s]", name, tgt, out)
+	}
+}
+
+// FuzzReflow does the same thing as TestReflow but with fuzzing
+func FuzzReflow(f *testing.F) {
+	const testdir = "testdata"
+	fis, err := os.ReadDir(testdir)
+	must(err)
+	for _, fi := range fis {
+		buf, _ := os.ReadFile(filepath.Join(testdir, fi.Name()))
+		v := strings.Split(string(buf), "\n====\n")
+		in := v[0]
+		f.Add(in, 75)
+	}
+	f.Fuzz(func(t *testing.T, in string, sz int) {
+		reflow(in, sz)
+	})
+}
